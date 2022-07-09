0a5a58ebf5a919c931c36cfe3a23eed1e4168961
shobhit <shobhitjoshi28march@gmail.com>
2022-07-09 21:16:07 +0530

    Edited categories page

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -7,13 +7,27 @@ import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArro
 import { NavLink } from 'react-router-dom'
 import './Categories.css'
 import SearchIcon from '@mui/icons-material/Search';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Categories = () => {
+	const modalOpen = () => {
+		const modal = document.getElementById('modal');
+		const container = document.getElementById('Container');
+		container.style.opacity = '0.3'
+		modal.style.display = 'flex'
+	}
+	const modalClose = () => {
+		const modal = document.getElementById('modal');
+		const container = document.getElementById('Container');
+		container.style.opacity = '1'
+		modal.style.display = 'none'
+	}
 	return (
 		<div className="notification_main">
 			<Sidebar />
 			<div style={{ width: '174px' }}></div>
-			<div className="box">
+			<div className="box" id="Container">
 				<Navbar />
 				<div className="categoriesPage">
 					<div className="sortAndActions">
@@ -21,6 +35,9 @@ const Categories = () => {
 						<div className="searchBox">
 							<input type="text" name="filter" id="filter" />
 							<SearchIcon />
+						</div>
+						<div className="addCategoryBtn" onClick={() => modalOpen(this)}>
+							<span>Add Category</span>
 						</div>
 						{/* <div className="sortBox">
                             <select name="sorting" id="sorting">
@@ -48,6 +65,28 @@ const Categories = () => {
 						<NavLink to='#'>3</NavLink>
 						<KeyboardDoubleArrowRightIcon />
 					</div>
+				</div>
+			</div>
+			<div id="modal">
+				<h2>Add Category</h2>
+				<div className="editFields2">
+					<div className="addCategory">
+						<select>
+							<option value="Select Category">Select Category</option>
+							<option value="food">Food</option>
+							<option value="electronics">Electronics</option>
+							<option value="decorative">Decorative</option>
+							<option value="dailyUse">DailyUse</option>
+							<option value="grocery">Grocery</option>
+						</select>
+					</div>
+					<div className="addCategory">
+						<input type="file" />
+					</div>
+				</div>
+				<div className="editAndCloseBtn">
+					<div className="editBtn"><AddCircleOutlineIcon /> Add</div>
+					<div className="closeBtn" onClick={() => modalClose()}><CloseIcon /> Close</div>
 				</div>
 			</div>
 		</div>
